3f348bbd3b9885076699eaeccf78a8809240cba9
Ashish8077 <145583844+Ashish8077@users.noreply.github.com>
2023-09-21 11:37:33 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log("2">1); //print true 
+// console.log("02">1); //print true 
+
+/*
+javascript automatically convert strig value to number 
+"2" converted to 2
+*/
+
+// console.log(null>0); //false
+// console.log(null == 0); //false
+// console.log(null >=0) //true
+
+/*
+The reason is that an equality check == and comparisons (< < >= <=)
+works differently
+Comparisons convert null to a number ,treating it as 0. That's why
+null >=0 is true and null>0 is false.
+*/
+
+// Stric equality operator vs equality operator 
+console.log("2" == 2) //prints true due to automatic type conversion
+console.log("2" === 2) //prints false because Stric equlaity operator check both value and their data type 
