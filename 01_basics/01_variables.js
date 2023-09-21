b40d161eac91d4c4371433e98584e19771a10989
Ashish8077 <145583844+Ashish8077@users.noreply.github.com>
2023-09-21 04:42:28 +0000

    1_variable in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+var firstName = "ashish";
+let lastName = "pawar";
+
+email = "ashishpawar9833@gmail.com";
+const accountNo = "374800344";
+
+//Declaring variable not initializing value 
+let dob 
+
+
+/*
+  =>you cannot redeclare constant  and cannot assing any value once the variable is initialize
+  => you cannot simply use constant variable after declaration you need to initialize value to variable 
+*/
+// accountNo = "4598340033"
+
+
+var firstName = "Rahul"
+lastName = "Panwar"  // let cannot be redeclare  but can reassing it's value 
+/*
+  =>You can change the value of  variable declare using let and var keyword 
+  =>only difference that var has global scope and let has block scope  when declare inside a particular block of code
+
+*/
+
+console.table([firstName , lastName ,email , accountNo ,dob])
+
