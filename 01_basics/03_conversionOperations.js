23a66e953c6a3995666d8e771454335df23088b1
Ashish8077 <145583844+Ashish8077@users.noreply.github.com>
2023-09-21 08:42:31 +0000

    opration and prefix postfix in Javascript

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,14 +1,49 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
 let stringValue = "33";
 let strvalue2 = "33abc";
 let isLoggedIn = true
 
 // Converting String DataType To Nubmber 
 let numberValue = Number(stringValue);
-console.log(typeof numberValue);
-let numvalue2 = Number(strvalue2);
-console.log(typeof numvalue2); // return number
-console.log(numvalue2); //return NaN(Not A Number)
+// console.log(typeof numberValue);
 
-// Convertin  boolean to number 
+let numvalue2 = Number(strvalue2);
+// console.log(typeof(numvalue2)); 
+//=>return number
+
+// console.log(numvalue2);
+//=>return NaN(Not A Number)
+
+// Converting  boolean to number 
 let num = Number(isLoggedIn)
 console.log(typeof num , num)
+
+// ******************** Operations ********************
+
+let str1 = "Ashish"
+let str2 = " Pawar"
+let str3 = str1 + str2;
+console.log(str3)
+
+console.log("1" + 2+44+5+8) 
+//output => 124458
+//number written after string automatically convert into string
+
+console.log(1+2+"4")
+//output => 34
+//number written before string automatically convert into number 
+
+// ******************* Expressions - Increment operator ********************
+// postfix
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+//prefix
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
