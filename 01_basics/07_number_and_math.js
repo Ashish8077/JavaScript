56cf9cdd040009220ae0e50d1fe2bc209ea5293d
Ashish8077 <145583844+Ashish8077@users.noreply.github.com>
2023-09-22 16:03:54 +0000

    Number and Math in Javascript

---
+++ 01_basics/07_number_and_math.js
@@ -0,0 +1,31 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// ******************** Number ********************/
+const score = 400
+// console.log(score.toFixed(2)) //print 400.00
+const accountBal = new Number(40)
+// console.log(accountBal.toString().length)
+
+const number = 1234.533
+// console.log(number) //print 1234.533
+// console.log(number.toPrecision(4)) //print 1235
+
+const hundres = 10000000
+// console.log(hundres.toLocaleString('en-IN'))
+
+// ******************** Math ********************/
+
+console.log(Math);
+console.log(Math.abs(-4))
+console.log(Math.round(9.43))
+console.log(Math.ceil(4.1))
+console.log(Math.floor(4.9))
+console.log(Math.min(34,55,7,88))
+console.log(Math.max(44,3,44,88,88))
+console.log(Math.floor(Math.random()*10)+1)
+
+//
+const max = 20;
+const min = 10;
+console.log(Math.floor(Math.random() *(max-min+1))+min)
